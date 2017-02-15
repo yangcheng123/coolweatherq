efa2f8b3bd21ba9544c4dbb43523fd9009179744
zhouxu <842734161@qq.com>
2017-02-15 19:41:34 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/feicui/coolweatherq/db/Province.java
@@ -0,0 +1,32 @@
+package com.feicui.coolweatherq.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/2/15.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    private int getId(){
+        return id;
+
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
